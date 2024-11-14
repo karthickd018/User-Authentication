8528f44162ddf4a58ca9c46d72a59aad8babe30a
Karthick D <karthickd.018@gmail.com>
2024-11-14 09:45:41 +0530

    first commit

--- client-login-regi/src/App.jsx
+++ client-login-regi/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Home from "./components/home";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 
 function App() {
